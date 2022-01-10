d58250d4b581469e7376bf68f02e3bd791e029d4
nadiaf85 <nadia.fiorio@alice.it>
2022-01-10 15:45:43 +0100

    quasi finito

--- main.js
+++ main.js
@@ -30,3 +30,38 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+
+//Creo click al bottone
+
+let bottone = document.getElementById('addMemberButton');
+bottone.addEventListener ('click',datiTeam);
+
+
+//Creo funzione con i dati del Team
+
+function datiTeam(){
+  for (let i =0; i <team.length; i++){
+    let membriCorrenti = team[i];
+    aggiuntaNuoveCard(membriCorrenti);
+  }
+};
+
+
+//Creo funzione per aggiungere nuove card al team
+
+function aggiuntaNuoveCard (name,role,image){
+  let nuovaCard = `<div class="team-card">
+                    <div class="card-image">
+                      <img
+                        src="img/${image}"
+                        alt="${name}"
+                        />
+                      </div>
+                      <div class="card-text">
+                        <h3>${name}</h3>
+                        <p>${role}</p>
+                      </div>
+                    </div>`;
+document.querySelector('.team-container').innerHTML += nuovaCard;
+}
